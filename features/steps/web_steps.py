1e39e3ffbbbbb6adb4fda5c970344c77f70a8cc0
rwby <56618279+aramos20@users.noreply.github.com>
2024-07-16 23:26:39 -0400

    Update web_steps.py

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -133,7 +133,6 @@ def step_impl(context, message):
         )
     )
     assert(found)
-
 ##################################################################
 # This code works because of the following naming convention:
 # The id field for text input in the html is the element name
